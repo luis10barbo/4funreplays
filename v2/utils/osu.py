b21cfbb5f7065a4ba7582149c84969a11b5305e3
luis10barbo <luis10barbo2020@gmail.com>
2025-01-24 12:46:27 -0300

    Fix logic in getting mods

--- v2/utils/osu.py
+++ v2/utils/osu.py
@@ -47,7 +47,7 @@ def get_mods(replay_data: Replay):
     
     replay_mods_string = ""
     for mod in mods:
-        if mod is "Doubletime" or "Nightcore" or "HardRock":
+        if mod is "DoubleTime" or mod is "NightCore" or mod is "HardRock":
             continue
         replay_mods_string += mods_simplified[mod]
 
