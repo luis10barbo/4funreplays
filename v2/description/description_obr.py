0a0632147b40faf0a956767f91d1eb98976411d8
luis10barbo <luis10barbo2020@gmail.com>
2025-01-24 12:46:02 -0300

    Fix saving description with ? symbol

--- v2/description/description_obr.py
+++ v2/description/description_obr.py
@@ -31,7 +31,7 @@ Danser: https://github.com/Wieku/danser-go"""
     
     file_name = f"{replay_number} - {nickname} {map_data.song_title} [{map_data.difficulty}]".replace("\"", "").replace("'", "").replace("/", "")
 
-    description_path = os.path.join(PATH_DESCRIPTION_FOLDER_FILES, f"{file_name if file_name_as_title else replay_number}.txt")
+    description_path = os.path.join(PATH_DESCRIPTION_FOLDER_FILES, f"{file_name if file_name_as_title else replay_number}.txt").replace("?", "_")
     with open(description_path, "w", encoding="utf-8") as file:
         file.write(content)
     
